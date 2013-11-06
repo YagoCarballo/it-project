a7a8d6cc5b24dc7f9b04bdedc0f620a76b4d89c1
stewiedouglas <sudouglas@dundee.ac.uk>
2013-11-06 14:39:36 +0000

    Web config now connects to database and about reads string from temporary test table.

--- ITproject website/About/About.aspx
+++ ITproject website/About/About.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="About.aspx.cs" Inherits="About_About" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="About.aspx.cs" Inherits="About_Default" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
@@ -6,12 +6,12 @@
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
     <div class="content_holder">
-    
-        <asp:Label ID="lblTitle" runat="server" Text="Label"></asp:Label>
-    
+
+        <div id = "page">
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <asp:Panel ID="pagePanel" runat="server">
+            </asp:Panel>       
+        </div>
     </div>
- 
-
-
 </asp:Content>
 
--- ITproject website/About/About.aspx.cs
+++ ITproject website/About/About.aspx.cs
@@ -4,17 +4,51 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;             // Needed for ConfigurationManager class
+using System.Data;                      // Needed for DataTable
+using System.Data.SqlClient;            // Needed for SqlConnection
 
-public partial class About_About : System.Web.UI.Page
+public partial class About_Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        // Open database connection
+        SqlConnection conn = new SqlConnection();
+        string connString = ConfigurationManager.ConnectionStrings["midlindb"].ConnectionString;   
+        conn.ConnectionString = connString;
+        conn.Open();
 
+
+
+        
+
+        // Prepare and execute SQL command
+        SqlCommand comm = new SqlCommand("SELECT [test] FROM [Table]", conn);
+        SqlDataAdapter adapter = new SqlDataAdapter(comm);
+
+        // Set up a 'DataReader' object
+        SqlDataReader Table = comm.ExecuteReader();
+
+        // Now insert each row in 'results' into 'panelFilms'
+        while (Table.Read())
+        {
+            Label title = new Label();
+            title.Text = Table[0].ToString();
+            title.Width = 400;
+            pagePanel.Controls.Add(title);
+        }    
+
+
+       //  SELECT [test] FROM [Table]
+
+
+        // Close connection once we're finished with it
+        conn.Close();
     }
     protected override void OnPreRender(EventArgs e)
     {
         base.OnPreRender(e);
-        this.lblTitle.Text = "Text changed in OnPreRender event";
+        
 
     }
 }
