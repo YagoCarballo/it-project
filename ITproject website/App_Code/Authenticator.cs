4fe684274fabecd808aae8ee6bc80f0674ab4bb0
skyride <skylinerspeeder@gmail.com>
2013-11-14 13:24:33 +0000

    Updated Program Code

---
+++ ITproject website/App_Code/Authenticator.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+/// <summary>
+/// Summary description for Authenticator
+/// </summary>
+public class Authenticator
+{
+    //Attributes
+    private SqlConnection db;
+    private DataFactory dataFac;
+    private User user;
+
+	public Authenticator(SqlConnection db)
+	{
+        this.db = db;
+        dataFac = new DataFactory(db);
+        user = null;
+	}
+
+
+    //Check a session key
+    public bool checkSessionKey(string key)
+    {
+        if (db.State != ConnectionState.Open)
+        {
+            try
+            {
+                db.Open();
+            }
+            catch (SqlException ex)
+            {
+                return false;
+            }
+        }
+
+        //Build query
+        DateTime now = DateTime.Now;
+        SqlCommand cmd = new SqlCommand("SELECT user FROM MidLin.sessions WHERE key = @key AND expires < @now", db);
+        cmd.Parameters.AddWithValue("key", key);
+        cmd.Parameters.AddWithValue("now", now);
+
+        int userId;
+
+        using (SqlDataReader rdr = cmd.ExecuteReader())
+        {
+            if (rdr.HasRows)
+            {
+                rdr.Read();
+
+                userId = rdr.GetInt32(0);
+
+                rdr.Close();
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Fill the private user object with the user specified
+        user = dataFac.getUser(userId);
+
+        //Check it was found
+        if (user != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+    //Check login credentials
+    public bool checkLoginCredentials(string username, string password)
+    {
+        if (db.State != ConnectionState.Open)
+        {
+            try
+            {
+                db.Open();
+            }
+            catch (SqlException ex)
+            {
+                return false;
+            }
+        }
+
+        //Build query
+        SqlCommand cmd = new SqlCommand("SELECT user FROM MidLin.users WHERE username = @username AND password @password", db);
+        cmd.Parameters.AddWithValue("username", username);
+        cmd.Parameters.AddWithValue("now", sha256(password));
+
+        int userId;
+
+        using (SqlDataReader rdr = cmd.ExecuteReader())
+        {
+            if (rdr.HasRows)
+            {
+                rdr.Read();
+
+                userId = rdr.GetInt32(0);
+
+                rdr.Close();
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Get user
+        user = dataFac.getUser(userId);
+        if (user != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //Sha hash method
+    private string sha256(string input)
+    {
+        byte[] bytes = new byte[input.Length * sizeof(char)];
+        System.Buffer.BlockCopy(input.ToCharArray(), 0, bytes, 0, bytes.Length);
+        byte[] result;
+
+        using (SHA256 shaM = new SHA256Managed())
+        {
+            result = shaM.ComputeHash(bytes);
+            ASCIIEncoding ascii = new ASCIIEncoding();
+            return ascii.GetString(result, 0, 256);
+        }
+    }
+}
---
+++ ITproject website/App_Code/Gallery.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Gallery
+/// </summary>
+public class Gallery
+{
+    //Attributes
+    private int id;
+    private DateTime created;
+    private User createdBy;
+    private string name;
+    private string description;
+    private List<Image> images;
+
+
+	public Gallery(int id, DateTime created, User createdBy, string name, string description, List<Image> images)
+	{
+        this.id = id;
+        this.created = created;
+        this.createdBy = createdBy;
+        this.name = name;
+        this.description = description;
+        this.images = images;
+	}
+
+
+    //Get image
+    public Image getImage(int id)
+    {
+        //Check if value is valid
+        if (id < 0 && id >= images.Count)
+        {
+            return null;
+        }
+
+        return images[id];
+    }
+
+
+    //Number if images in the gallery
+    public int getImageCount()
+    {
+        return images.Count;
+    }
+
+
+    //Getters
+    public int getId() { return id; }
+    public DateTime getCreated() { return created; }
+    public User getCreatedBy() { return createdBy; }
+    public string getName() { return name; }
+    public string getDescription() { return description; }
+}
