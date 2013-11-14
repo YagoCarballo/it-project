6f6f70b76ca3a427e6b8fdcbc2b7b2388a4705cf
stewiedouglas <sudouglas@dundee.ac.uk>
2013-11-14 15:16:39 +0000

    cleaned css and cleaned pages

--- ITproject website/News/News.aspx.cs
+++ ITproject website/News/News.aspx.cs
@@ -9,6 +9,21 @@ public partial class News_News : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        // Open database connection
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["midlindb"].ConnectionString);
+        conn.Open();
+        DataFactory dataFac = new DataFactory(conn);
+
+        Page page = dataFac.getPage(1);
+
+        title.Text = page.getTitle();
+        content.Text = page.getContent();
+        author.Text = "by " + page.getUser().getFullName() + " on " + page.getCreated().Date;
+    }
+    protected override void OnPreRender(EventArgs e)
+    {
+        base.OnPreRender(e);
+
 
     }
 }
